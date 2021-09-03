b96ebc93d0d5d055eecd09c250c884cddfafae04
Felipe <felipe.seo@fatec.sp.gov.br>
2021-09-03 10:57:42 -0300

    Status pt1

--- src/components/RegisterSuppliers.jsx
+++ src/components/RegisterSuppliers.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./RegisterSuppliers.css"
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import {TitleLogin} from "./Titles.jsx";
@@ -8,6 +8,17 @@ import {SendButton, ClearButton} from "./Buttons.jsx";
 // import { Container } from './styles';
 
 function RegisterSuppliers() {
+  const [name, setName] = useState("");
+  const [document, setDocument] = useState("");
+  const [responsible_person, setResponsible_person] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone_ddd, setPhone_ddd] = useState(0);
+  const [phone, setPhone] = useState(0);  
+  const [address, setAddress] = useState("");
+  const [zip_code, setZip_code] = useState(0);
+  const [city, setCity] = useState("");
+  const [uf, setUf] = useState("");
+
   return (
       <>
 
@@ -15,25 +26,25 @@ function RegisterSuppliers() {
 <Formik>
     <Form className="FormRegisterSupplier">
         <Field type="text" name="name" className="FormSuppliers FormSuppliers-S1" placeholder="Fornecedor"/>
-        <ErrorMessage name="user" component="div" />
+        <p>{setName}</p>
         <Field type="text" name="document" className="FormSuppliers FormSuppliers-S2" maxlength="20" placeholder="CNPJ/CPF"/>
-        <ErrorMessage name="document" component="div" />
+        
         <Field type="text" name="responsible_person" className="FormSuppliers FormSuppliers-S1" maxlength="20" placeholder="Responsável"/>
-        <ErrorMessage name="responsible_person" component="div" />
+        
         <Field type="email" name="email" className="FormSuppliers FormSuppliers-S2" maxlength="80" placeholder="E-mail"/>
-        <ErrorMessage name="E-mail" component="div" />
+        
         <Field type="number" name="phone_ddd" className="FormSuppliers FormSuppliers-S3" maxlength="3" placeholder="DDD"/>
-        <ErrorMessage name="DDD" component="div" />
+        
         <Field type="number" name="phone_number" className="FormSuppliers FormSuppliers-S4" maxlength="9" placeholder="Telefone"/>
-        <ErrorMessage name="phone_number" component="div" />
+        
         <Field type="text" name="address" className="FormSuppliers FormSuppliers-S1" maxlength="250" placeholder="Endereço"/>
-        <ErrorMessage name="address" component="div" />   
+        
         <Field type="number" name="zip_code" className="FormSuppliers FormSuppliers-S2" maxlength="9" placeholder="CEP"/>
-        <ErrorMessage name="zip_code" component="div" />  
+        
         <Field type="text" name="city" className="FormSuppliers FormSuppliers-S4" maxlength="80" placeholder="Cidade"/>
-        <ErrorMessage name="city" component="div" />     
+        
         <Field type="text" name="uf" className="FormSuppliers FormSuppliers-S3" maxlength="2" placeholder="UF"/>
-        <ErrorMessage name="uf" component="div" />  
+        
         
         <ClearButton nameButton="Limpar"/>
         <SendButton nameButton="Enviar"/>
